d3889c47f9628abe018ef430099c1b9b65a5e940
Ermo Wei <nosyndicate@gmail.com>
2026-05-25 01:13:09 -0700

    CR feedback

--- server/model/utils.py
+++ server/model/utils.py
@@ -9,14 +9,14 @@ def bytes_to_gb(bytes_value: int) -> str:
     return f"{gb:.2f} GB"
 
 
-def get_available_memory(memory_utilization: float) -> float:
+def get_available_memory(memory_utilization: float) -> int:
     """Returns the available GPU memory in bytes."""
     free_mem, total_mem = torch.cuda.mem_get_info()
     total_free_mem = free_mem * memory_utilization
     peak_mem_usage = torch.cuda.memory_stats()["allocated_bytes.all.peak"]
     current_mem_usage = torch.cuda.memory_stats()["allocated_bytes.all.current"]
     # reserve some room for peak memory usage during model execution
-    available_mem = total_free_mem - (peak_mem_usage - current_mem_usage)
+    available_mem = max(0, int(total_free_mem - (peak_mem_usage - current_mem_usage)))
     log_event(f"available_mem: {bytes_to_gb(available_mem)}")
 
     return available_mem
