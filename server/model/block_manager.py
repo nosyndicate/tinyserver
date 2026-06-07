7391b1bcef26abcd8d1df1559a57de3beb633a6b
Ermo Wei <nosyndicate@gmail.com>
2026-06-07 14:56:06 -0700

    cr feedback

--- server/model/block_manager.py
+++ server/model/block_manager.py
@@ -1,4 +1,4 @@
-import math
+import heapq
 
 from server.executor.types import Sequence
 
@@ -15,18 +15,21 @@ class BlockManager:
     def __init__(self, total_blocks: int, block_size: int):
         self.total_blocks = total_blocks
         self.block_size = block_size
-        self.free_blocks = set(range(total_blocks))
+
+        # free_blocks is a min-heap of available block IDs. We use a heap to efficiently allocate and free blocks.
+        self.free_blocks = list(range(total_blocks))
+        heapq.heapify(self.free_blocks)
         self.allocated_blocks: dict[
             str, set[int]
         ] = {}  # sequence_id -> set of block_ids
 
     def _num_blocks_needed(self, num_tokens: int) -> int:
-        return int(math.ceil(num_tokens / self.block_size))
+        return (num_tokens + self.block_size - 1) // self.block_size  # Ceiling division
 
     def can_allocate(self, sequence: Sequence) -> bool:
         """Checks if the requested sequence can be allocated given the current free blocks."""
-        num_blocks = self._num_blocks_needed(sequence.num_tokens)
-        return len(self.free_blocks) >= num_blocks
+        num_blocks_needed = self._num_blocks_needed(sequence.num_tokens)
+        return len(self.free_blocks) >= num_blocks_needed
 
     def can_append(self, sequence: Sequence) -> bool:
         """Checks if we can append one more token to the sequence."""
@@ -41,10 +44,10 @@ class BlockManager:
         if not self.can_allocate(sequence):
             raise MemoryError("Not enough free blocks to allocate")
 
-        num_blocks = self._num_blocks_needed(sequence.num_tokens)
+        num_blocks_needed = self._num_blocks_needed(sequence.num_tokens)
         allocated = set()
-        for _ in range(num_blocks):
-            block_id = self.free_blocks.pop()
+        for _ in range(num_blocks_needed):
+            block_id = heapq.heappop(self.free_blocks)
             allocated.add(block_id)
 
         self.allocated_blocks[sequence.sequence_id] = allocated
@@ -56,13 +59,16 @@ class BlockManager:
 
         if sequence.num_tokens % self.block_size == 0:
             # Need to allocate a new block
-            block_id = self.free_blocks.pop()
+            block_id = heapq.heappop(self.free_blocks)
             self.allocated_blocks[sequence.sequence_id].add(block_id)
             sequence.block_table.append(block_id)
 
     def free(self, sequence: Sequence) -> None:
+        """
+        Clears the blocks allocated to the sequence and returns them to the free pool.
+        """
         allocated = self.allocated_blocks.pop(sequence.sequence_id, set())
         for block_id in allocated:
-            self.free_blocks.add(block_id)
+            heapq.heappush(self.free_blocks, block_id)
 
         sequence.block_table = []
